75bb08e3521ebd33516cb0462081dac21e2b997b
Leslie Pajuelo <l.l.pajuelo@gmail.com>
2015-11-26 16:29:31 -0800

    Envision files

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + '/app'));
+app.use(express.static(process.cwd() + '/app'));
+app.use(express.static(__dirname + '/node/_modules'));
 app.listen(process.env.PORT || 3000);
---
+++ app/Table/table.js
@@ -0,0 +1,30 @@
+'use strict';
+
+angular.module('myApp.Table', ['ngRoute', 'smart-table'])
+
+.config(['$routeProvider', function($routeProvider, $http) {
+  $routeProvider.when('/table', {
+    templateUrl: 'Table/table.html',
+    controller: 'filterCtrl'
+  });
+}]).controller('filterCtrl', ['$scope', '$filter', function ($scope, filter) {
+  $scope.sortType = "AREA_NAME";
+  $scope.sortRevers = false;
+  $scope.searchLocation="";
+
+
+
+  //http://lorenzofox3.github.io/smart-table-website/
+    // scope.predicates = ['Areaname', 'Rent50_3'];
+    // scope.selectedPredicate = scope.predicates[0];
+}]).controller("PostsCtrl", function($scope, $http) {
+  //Pulling in housing data, incomplete
+  $http.get('assets/joined.json').
+    success(function(data) {
+      $scope.posts = data;
+    }).
+    error(function(data) {
+      // log error
+    });
+
+});
