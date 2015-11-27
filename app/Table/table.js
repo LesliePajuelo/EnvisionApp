d3b16db478f6672f07da6770eb1ceb2245eab222
Leslie Pajuelo <l.l.pajuelo@gmail.com>
2015-11-26 21:27:05 -0800

    change state tag, partial pop filter

--- app/Table/table.js
+++ app/Table/table.js
@@ -7,7 +7,7 @@ angular.module('myApp.Table', ['ngRoute', 'smart-table'])
     templateUrl: 'Table/table.html',
     controller: 'filterCtrl'
   });
-}]).controller('filterCtrl', ['$scope', '$filter', function ($scope, filter) {
+}]).controller('filterCtrl', ['$scope', '$filter', '$timeout', function ($scope, filter) {
   $scope.sortType = "AREA_NAME";
   $scope.sortRevers = false;
   $scope.searchLocation="";
@@ -17,7 +17,7 @@ angular.module('myApp.Table', ['ngRoute', 'smart-table'])
   //http://lorenzofox3.github.io/smart-table-website/
     // scope.predicates = ['Areaname', 'Rent50_3'];
     // scope.selectedPredicate = scope.predicates[0];
-}]).controller("PostsCtrl", function($scope, $http) {
+}]).controller("PostsCtrl", function($scope, $http, $timeout) {
   //Pulling in housing data, incomplete
   $http.get('assets/joinedData.json').
     success(function(data) {
@@ -28,4 +28,25 @@ angular.module('myApp.Table', ['ngRoute', 'smart-table'])
       // log error
     });
 
+    $scope.popSlider = {min:5000, max:13000000}
+
+}).filter('population', function($timeout){
+  var delayedFilter = function(){
+  
+  console.log('timeout');
+  
+  return function(posts, greaterThan, lessThan){
+    posts = posts.filter(function(post){
+    return posts.pop2010 > greaterThan && posts.pop2010 < lessThan;
+    });
+    
+    return posts;
+  }; 
+};
+
+  $timeout(function(){
+    delayedFilter()
+  }, 3000)
+
+  return delayedFilter();
 });
